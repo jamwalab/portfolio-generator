073ed45623cc6b3477d2d282943649a0c954a69b
Abhishek Jamwal <jamwal.ab@gmail.com>
2021-06-11 23:58:04 -0400

    inquirer updated

--- app.js
+++ app.js
@@ -24,13 +24,82 @@ printProfileData(profileDataArgs);*/
 
     console.log("Portfolio complete!! Checkout index.html to see the output!");
 });*/
-
-inquirer
+const promptUser = () => {
+    return inquirer
     .prompt([
         {
             type: "input",
             name: "name",
             message: "What is your name?"
+        },
+        {
+            type: "input",
+            name: "github",
+            message: "Enter your github username"
+        },
+        {
+            type: "input",
+            name: "about",
+            message: "Provide some information about yourself"
         }
-    ])
-    .then(answers => console.log(answers));
+    ]);
+};
+
+const promptProject = portfolioData => {
+    if (!portfolioData.projects) {
+        portfolioData.projects = [];
+    }
+    console.log(`
+    =================
+    Add a New Project
+    =================
+    `);
+    return inquirer.prompt([
+        {
+            type: "input",
+            name: "name",
+            message: "What is the name of your project"
+        },
+        {
+            type: "input",
+            name: "description",
+            message: "Provide a description of your project (required)"
+        },
+        {
+            type: "checkbox",
+            name: "Languages",
+            message: "What did you build this project with (check all that apply)",
+            choices: ["JavaScript", "HTML", "CSS", "ES6", "jQuery", "Bootstrap", "Node"]
+        },
+        {
+            type: "input",
+            name: "link",
+            message: "Enter the GitHub link to your project. (Required)"
+        },
+        {
+            type: "confirm",
+            name: "feature",
+            message: "Would you like to feature this project?",
+            default: false
+        },
+        {
+            type: "confirm",
+            name: "confirmAddProject",
+            message: "Would you like to enter another project?",
+            default: false
+        },
+    ]).then(projectData => {
+        portfolioData.projects.push(projectData);
+        if (projectData.confirmAddProject) {
+            return promptProject(portfolioData);
+        } else {
+            return portfolioData;
+        }
+    });
+}
+
+promptUser()
+    .then(promptProject)
+    .then(portfolioData => {
+        console.log(portfolioData);
+    });
