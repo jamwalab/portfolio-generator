dd1c64a3afa591d3c037eadce9d536670244a491
Abhishek Jamwal <jamwal.ab@gmail.com>
2021-06-11 22:45:13 -0400

    inquirer added

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
-const fs = require("fs");
+const inquirer = require("inquirer");
+/*const fs = require("fs");
 const generatePage = require("./src/page-template.js");
-const profileDataArgs = process.argv.slice(2);
+
+const pageHTML = generatePage(name,github)*/
+//const profileDataArgs = process.argv.slice(2);
 //const name = profileDataArgs[0];
 //const github = profileDataArgs[1];
-const [name, github] = profileDataArgs;
+//const [name, github] = profileDataArgs;
 /*const printProfileData = (profileDataArr) => {
     for(let i=0; i<profileDataArr.length; i++) {
         console.log(profileDataArr[i]);
@@ -16,8 +19,18 @@ const [name, github] = profileDataArgs;
 
 printProfileData(profileDataArgs);*/
 
-fs.writeFile("./index.html", generatePage(name,github), err => {
+/*fs.writeFile("./index.html", pageHTML, err => {
     if (err) throw err;
 
     console.log("Portfolio complete!! Checkout index.html to see the output!");
-});
+});*/
+
+inquirer
+    .prompt([
+        {
+            type: "input",
+            name: "name",
+            message: "What is your name?"
+        }
+    ])
+    .then(answers => console.log(answers));
